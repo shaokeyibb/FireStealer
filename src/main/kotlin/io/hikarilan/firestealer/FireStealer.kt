af8638c67b1e6690df0a7528cd9550b8d1856ef3
HikariLan <shaokeyibb@126.com>
2022-05-07 20:18:21 +0800

    Initial commit

---
+++ src/main/kotlin/io/hikarilan/firestealer/FireStealer.kt
@@ -0,0 +1,16 @@
+package io.hikarilan.firestealer
+
+import net.minecraftforge.fml.common.Mod
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+@Mod(FireStealer.MOD_ID)
+object FireStealer {
+
+    const val MOD_ID = "firestealer"
+
+    val LOGGER: Logger = LogManager.getLogger(MOD_ID)
+
+    // 看啥看，一个模板项目而已，你还想要他有什么东西？
+
+}
