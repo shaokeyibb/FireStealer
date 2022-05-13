04bd7f3a9764b79485b5343be6d148385253bd27
HikariLan <shaokeyibb@126.com>
2022-05-13 22:44:39 +0800

    improve phlogiston-rich raw ore tooltip display

--- src/main/kotlin/io/hikarilan/firestealer/items/PhlogistonRichRawOreItem.kt
+++ src/main/kotlin/io/hikarilan/firestealer/items/PhlogistonRichRawOreItem.kt
@@ -1,12 +1,17 @@
 package io.hikarilan.firestealer.items
 
 import io.hikarilan.firestealer.FireStealerMod
+import net.minecraft.ChatFormatting
+import net.minecraft.core.NonNullList
+import net.minecraft.core.Registry
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.TranslatableComponent
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.TagKey
 import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.item.ItemEntity
-import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Rarity
+import net.minecraft.world.item.*
 import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.Level
 import net.minecraftforge.registries.ForgeRegistries
@@ -35,6 +40,39 @@ class PhlogistonRichRawOreItem : Item(
         if (!recipe.isPresent) return null
 
         return recipe.get().resultItem.copy().apply { count = entity.item.count }
+    }
+
+    override fun fillItemCategory(pCategory: CreativeModeTab, pItems: NonNullList<ItemStack>) {
+        if (allowdedIn(pCategory)) {
+            ForgeRegistries.ITEMS.tags()!!.getTag(
+                TagKey.create(
+                    Registry.ITEM_REGISTRY,
+                    ResourceLocation("firestealer:phlogiston_mixed_ores")
+                )
+            ).forEach { item ->
+                pItems.add(ItemStack(this).apply {
+                    this.tag = CompoundTag().also {
+                        it.putString("ore", ForgeRegistries.ITEMS.getKey(item).toString())
+                    }
+                })
+            }
+        }
+    }
+
+    override fun appendHoverText(
+        pStack: ItemStack,
+        pLevel: Level?,
+        pTooltipComponents: MutableList<Component>,
+        pIsAdvanced: TooltipFlag
+    ) {
+        val ore = ForgeRegistries.ITEMS.getValue(ResourceLocation(pStack.tag?.getString("ore") ?: return))?.description
+            ?: return
+        pTooltipComponents.add(
+            TranslatableComponent(
+                "item.firestealer.phlogiston_rich_raw_ore.tooltip",
+                ore
+            ).withStyle(ChatFormatting.BOLD, ChatFormatting.GOLD)
+        )
     }
 
 }
